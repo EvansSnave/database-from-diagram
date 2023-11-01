9f4a255e658fc30f7d398ecadf81641d172aa9f9
Ademola Owoeye <ademolaowoeye07@gmail.com>
2023-11-01 12:51:04 +0100

    created clinic database and re-created all required table in the clinic database .

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,53 +1,61 @@
-CREATE TABLE "patients" (
-  "id" integer PRIMARY KEY,
-  "name" varchar,
-  "date_of_birth" date
+--Create database
+CREATE DATABASE clinic;
+
+-- create patients table
+CREATE TABLE patients (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(255),
+    date_of_birth DATE,
+    PRIMARY KEY (id)
 );
 
-CREATE TABLE "medical_histories" (
-  "id" integer PRIMARY KEY,
-  "admitted_at" timestamp,
-  "patient_id" integer,
-  "status" varchar
+-- create the treatments table
+CREATE TABLE treatments (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    type VARCHAR(255),
+    name VARCHAR(255),
+    PRIMARY KEY (id)
 );
 
-CREATE TABLE "invoices" (
-  "id" integer PRIMARY KEY,
-  "total_amount" decimal,
-  "generated_at" timestamp,
-  "payed_at" timestamp,
-  "medical_histories_id" integer
+-- create medical_histories table
+CREATE TABLE medical_histories (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    admitted_at timestamp,
+    patient_id INT,
+    status VARCHAR(255),
+    PRIMARY KEY (id),
+    FOREIGN KEY (patient_id) REFERENCES patients (id)
 );
 
-CREATE TABLE "invoices_items" (
-  "id" integer PRIMARY KEY,
-  "unit_price" decimal,
-  "quantity" integer,
-  "total_price" decimal,
-  "invoice_id" integer,
-  "treatment_id" integer
+-- create invoices table
+CREATE TABLE invoices (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    total_amount DECIMAL(10,2),
+    generated_at timestamp,
+    payed_at timestamp,
+    medical_history_id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (medical_history_id) REFERENCES medical_histories (id),
 );
 
-CREATE TABLE "treatments" (
-  "id" integer PRIMARY KEY,
-  "type" varchar,
-  "name" varchar
+-- create invoice items table
+CREATE TABLE invoice_items (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    unit_price DECIMAL(10,2),
+    quantity INT,
+    total_price DECIMAL(10,2),
+    invoice_id INT,
+    treatement_id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (invoice_id) REFERENCES invoices (id),
+    FOREIGN KEY (treatement_id) REFERENCES treatments (id)
 );
 
-ALTER TABLE "medical_histories" ADD FOREIGN KEY ("patient_id") REFERENCES "patients" ("id");
-
-ALTER TABLE "invoices" ADD FOREIGN KEY ("medical_histories_id") REFERENCES "medical_histories" ("id");
-
-ALTER TABLE "invoices_items" ADD FOREIGN KEY ("invoice_id") REFERENCES "invoices" ("id");
-
-ALTER TABLE "invoices_items" ADD FOREIGN KEY ("treatment_id") REFERENCES "treatments" ("id");
-
-CREATE TABLE "treatments_medical_histories" (
-  "treatments_id" integer,
-  "medical_histories_id" integer,
-  PRIMARY KEY ("treatments_id", "medical_histories_id")
+-- create medical_histories and treatments join table
+CREATE TABLE medical_histories_treatments (
+    medical_history_id INT,
+    treatment_id INT,
+    PRIMARY KEY (medical_history_id, treatment_id),
+    FOREIGN KEY (medical_history_id) REFERENCES medical_histories (id),
+    FOREIGN KEY (treatment_id) REFERENCES treatments (id)
 );
-
-ALTER TABLE "treatments_medical_histories" ADD FOREIGN KEY ("treatments_id") REFERENCES "treatments" ("id");
-
-ALTER TABLE "treatments_medical_histories" ADD FOREIGN KEY ("medical_histories_id") REFERENCES "medical_histories" ("id");
